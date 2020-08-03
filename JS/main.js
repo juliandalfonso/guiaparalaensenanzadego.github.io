b03916384230e5317dd37e461958315f22306a8d
Julian Alfonso <guiaparalaensenanzadego@gmail.com>
2020-08-02 21:18:20 -0500

    se ha subido la primera version de la pagina web

---
+++ JS/main.js
@@ -0,0 +1,35 @@
+// AOSINIT--------------------------------------------------
+
+setTimeout(function() { aosInit(); }, 4000);
+
+// INTROJSINIT--------------------------------------------
+
+// introJs().start();
+// TERMINALSINIT----------------------------------------------
+var termynal = new Termynal('#termynal', { typeDelay: 10 })
+
+
+
+//QUERYSELECTORS---------------------------------------------
+    
+    
+
+//EVENTLISTENERS---------------------------------------------
+  
+
+//FUNCTIONS---------------------------------------------
+   
+function aosInit()
+{
+  AOS.init();
+}
+    
+
+
+//AJUSTA LA POSICION DEL NAVBAR FIXED-TOP
+$(document).ready(function(){
+  $(document.body).css('padding-top', $('#topnavbar').height() + 10);
+  $(window).resize(function(){
+      $(document.body).css('padding-top', $('#topnavbar').height() + 10);
+  });
+});
