9c13b7d237e34b062e79e9ff628e6e717e4f3fe4
Julian Alfonso <guiaparalaensenanzadego@gmail.com>
2020-08-04 16:47:55 -0500

    se distribuyeron los ejemplos en carpetas y se agregaron los primeros 6 ejemplos ahora se muestran en modales

--- JS/condicionales.js
+++ JS/condicionales.js
@@ -3,7 +3,8 @@ AOS.init();
 
 // CLIPBOARDJSINIT----------------------------------------------
 
-var copyclip1 = new ClipboardJS('#copyejemplo_1');
+//inicializamos clipboardjs para el ejemplo #copyejemplo_1 es un <a></a>
+var copyclipEjemplo1 = new ClipboardJS('#copyejemplo_1');
 
 // SMOOTHSCROLLING--------------------------------------------------
 var scroll = new SmoothScroll('a[href*="#"]', 
@@ -35,12 +36,12 @@ Sortable.create(simpleList, { /* options */ });
 
 
 //------------------------------ query selectors del ejemplo 1----------------------------------
-const runejemplo_1 = document.querySelector('#runejemplo_1');
-const debugejemplo_1 = document.querySelector('#debugejemplo_1');
-const copyejemplo_1 = document.querySelector('#copyejemplo_1');
+// const runejemplo_1 = document.querySelector('#runejemplo_1');
+// const debugejemplo_1 = document.querySelector('#debugejemplo_1');
+// const copyejemplo_1 = document.querySelector('#copyejemplo_1');
 
-// Selecciona todos los botones que haya que quitarle el default--------------------------------------
-const previeneDefault = document.querySelector('.prev');
+// // Selecciona todos los botones que haya que quitarle el default--------------------------------------
+// const previeneDefault = document.getElementsByClassName('prev');
 
 
 
@@ -63,10 +64,14 @@ const previeneDefault = document.querySelector('.prev');
 
 
 // event listeners ejemplo 1-------------------------------------------------------------------
-runejemplo_1.addEventListener('click', runnerejemplo_1);
-previeneDefault.addEventListener('click', prev);
-debugejemplo_1.addEventListener('click', debugerejemplo_1);
+// runejemplo_1.addEventListener('click', runnerejemplo_1);
 
+// debugejemplo_1.addEventListener('click', debugerejemplo_1);
+
+// //previene la funcion por defecto de todos los nodos seleccionados con la clase prev
+// for (var i=0;i<previeneDefault.length; i++) {
+//   previeneDefault[i].addEventListener('click', prev);
+// }
 
 
 
@@ -76,47 +81,45 @@ debugejemplo_1.addEventListener('click', debugerejemplo_1);
 
 
 
-//FUNCTIONS---------------------------------------------
+// //FUNCTIONS---------------------------------------------
 
-//crea un nuevo elemento en la terminal del ejemplo_1 y y lo agrega
-function runnerejemplo_1(e) {
+// //crea un nuevo elemento en la terminal del ejemplo_1 y y lo agrega
+// function runnerejemplo_1(e) {
   
-  e.preventDefault();
+//   e.preventDefault();
 
-  e.target.removeEventListener(e.type, arguments.callee);//borramos el event listener para que no se vuelva a ejecutar esta funcion
+//   e.target.removeEventListener(e.type, arguments.callee);//borramos el event listener para que no se vuelva a ejecutar esta funcion
  
-  document.getElementById("rundivejemplo_1").insertAdjacentHTML("afterend",
-    "<p class='separator'></p><span data-ty='input' data-ty-prompt=''><p id='prompt'>hello world!</p></span>");
-  document.querySelector('#prompt').className = 'animate__animated animate__bounce text-success ml-4';
-}
+//   document.getElementById("rundivejemplo_1").insertAdjacentHTML("afterend",
+//     "<p class='separator'></p><span data-ty='input' data-ty-prompt=''><p id='prompt'>hello world!</p></span>");
+//   document.querySelector('#prompt').className = 'animate__animated animate__bounce text-success ml-4';
+// }
 
 
-//Ejecuta IntroJS en el ejemplo_1 
-function debugerejemplo_1(e) {
-  e.preventDefault();
-  console.log('entreeeee');
-  introJs('#ejemplo_1').start();
-}
+// //Ejecuta IntroJS en el ejemplo_1 
+// function debugerejemplo_1(e) {
+//   e.preventDefault();
+//   introJs().start();
+// }
 
-//funcion que elimina todas las funciones por defecto 
-function prev(e) {
-  e.preventDefault();
-}
-
+// //funcion que elimina todas las funciones por defecto 
+// function prev(e) {
+//   e.preventDefault();
+// }
 
 
-// funcion que muestra mensaje "copiado!" al dar click en el boton copy
-tippy('#copyejemplo_1', {
-  content: 'copiado!',
-  trigger: 'click'
-});
+// // funcion que muestra mensaje "copiado!" al dar click en el boton copy
+// tippy('#copyejemplo_1', {
+//   content: 'copiado!',
+//   trigger: 'click'
+// });
 
 
 
-// funcion que borra la seleccion una vez copiada
-copyclip1.on('success', function (e) {
-  e.clearSelection();
-});
+// // funcion que borra la seleccion una vez copiada
+// copyclipEjemplo1.on('success', function (e) {
+//   e.clearSelection();
+// });
 
 
 
