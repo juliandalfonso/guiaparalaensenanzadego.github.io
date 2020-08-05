8ef1f9f93d4b2c35682bbe903a4d1b9b72d3e180
Julian Alfonso <guiaparalaensenanzadego@gmail.com>
2020-08-05 18:09:37 -0500

    se quito el z-index a los modulos para permitir el click a los botones de ver también se arregló el navbar con un codigo de javascript

--- JS/main.js
+++ JS/main.js
@@ -23,3 +23,15 @@ function aosInit()
 {
   AOS.init();
 }
+
+
+
+
+
+//codigo que corrige el navbar de hacer overflow
+    $(document).ready(function(){
+        $(document.body).css('padding-top', $('#topnavbar').height() + 10);
+        $(window).resize(function(){
+            $(document.body).css('padding-top', $('#topnavbar').height() + 10);
+        });
+    });
